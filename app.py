50de0c6dcbdedc1a31aa2899284cd6b2028aecbb
indraJuliansyahPutra <mrindrajuliansyahputra10@gmail.com>
2024-07-22 16:06:51 +0700

    Change directory

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 import joblib
 import matplotlib.pyplot as plt
 from PIL import Image
--- predict.py
+++ predict.py
@@ -1,26 +1,26 @@
+import pandas as pd
 import numpy as np
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+import joblib
 
 # Load the saved model
-model = load_model('gender_prediction.h5')
+model_file = 'model/nb.pkl'
+best_model_knn = joblib.load(model_file)
 
 # Function to preprocess a single name
 def preprocess_name(name, max_len=20):
     name = name.lower()
-    name_array = [max(0.0, ord(char) - 96.0) for char in name]
-    name_array = name_array + [0.0] * (max_len - len(name_array))  # Pad to max_len
+    name_array = list(name) + [' '] * (max_len - len(name))
+    name_array = [max(0.0, ord(char)-96.0) for char in name_array]
     return np.array(name_array).reshape(1, -1)
 
 # Predict function
 def predict_gender(name):
     name_array = preprocess_name(name)
-    prediction = model.predict(name_array)
-    predicted_class = int(round(prediction[0][0]))
-    return 'Laki-Laki' if prediction == 1 else 'Perempuan'
+    prediction = best_model_knn.predict(name_array)
+    return 'Laki-Laki' if prediction[0] == 1 else 'Perempuan'
 
 # Example prediction
-name = "Zee"  # Ganti dengan nama lain untuk prediksi
+name = "Putri"  # Ganti dengan nama lain untuk prediksi
 predicted_gender = predict_gender(name)
 print(f"Nama: {name}")
 print(f"Prediksi Jenis Kelamin: {predicted_gender}")
